9ff8abebd47b479ebdcf75cfffa2f527d0edbabd
Kevin Berendsen <10073027+kevto@users.noreply.github.com>
2023-09-09 17:19:32 +0200

    Starting point for the demo of the presentation

--- terraform/resources.tf
+++ terraform/resources.tf
@@ -1,33 +1,12 @@
 # Create password for the database root user
 
-resource "random_password" "db_root_password" {
-  length = 16
-
-  min_lower = 1
-  min_numeric = 1
-  min_special = 1
-  min_upper = 1
-}
+# TODO Random password
+# TODO See: https://registry.terraform.io/providers/hashicorp/random/latest/docs/resources/password
 
 # Setup the database
 
-resource "google_sql_database_instance" "jworld" {
-  name             = "jworld"
-  region           = "europe-west1"
-  database_version = "POSTGRES_15"
-  settings {
-    tier = "db-f1-micro"
-  }
-
-  root_password = random_password.db_root_password.result
-
-  deletion_protection  = "false" # Set to true on production
-}
-
-resource "google_sql_database" "jworld" {
-  name     = "jworld"
-  instance = google_sql_database_instance.jworld.name
-}
+# TODO Create instance and database
+# TODO See: https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/sql_database
 
 # Creating Cloud Run instances
 # Creating a policy to all everyone invoke the Cloud Run instances
@@ -44,7 +23,11 @@ data "google_iam_policy" "noauth" {
 # Api application
 
 # TODO Cloud Run service and IAM policy
+# TODO See: https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/cloud_run_v2_service#example-usage---cloudrunv2-service-sql
+# TODO See: https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/cloud_run_v2_service_iam#google_cloud_run_v2_service_iam_policy
 
 # Frontend application
 
 # TODO Cloud Run service and IAM policy
+# TODO See: https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/cloud_run_v2_service#example-usage---cloudrunv2-service-sql
+# TODO See: https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/cloud_run_v2_service_iam#google_cloud_run_v2_service_iam_policy
